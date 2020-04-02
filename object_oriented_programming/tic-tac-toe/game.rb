0aa57eb3ffc1b2f9be5fbca8f76ec22f66d4a6de
Cameron Nigro <cameron.nigro@gmail.com>
2020-04-02 10:35:18 -0400

    separate files

--- object_oriented_programming/tic-tac-toe/game.rb
+++ object_oriented_programming/tic-tac-toe/game.rb
@@ -1,108 +1,19 @@
-class Board
-  WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
-                  [[1, 5, 9], [3, 5, 7]]              # diagonals
-
-  def initialize
-    @squares = {}
-    reset
-  end
-
-  def []=(num, marker)
-    @squares[num].marker = marker
-  end
-
-  def unmarked_keys
-    @squares.keys.select { |key| @squares[key].unmarked? }
-  end
-
-  def full?
-    unmarked_keys.empty?
-  end
-
-  def someone_won?
-    !!winning_marker
-  end
-
-  def winning_marker
-    WINNING_LINES.each do |line|
-      squares = @squares.values_at(*line)
-      if three_identical_markers?(squares)
-        return squares.first.marker
-      end
-    end
-    nil
-  end
-
-  def reset
-    (1..9).each { |key| @squares[key] = Square.new() }
-  end
-
-  def draw
-    puts "     |     |"
-    puts "  #{@squares[1]}  |  #{@squares[2]}  |  #{@squares[3]}  "
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
-    puts "  #{@squares[4]}  |  #{@squares[5]}  |  #{@squares[6]}  "
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
-    puts "  #{@squares[7]}  |  #{@squares[8]}  |  #{@squares[9]}  "
-    puts "     |     |"
-  end
-
-  private
-
-  def three_identical_markers?(squares)
-    markers = squares.select(&:marked?).collect(&:marker)
-    return false if markers.size != 3
-    markers.min == markers.max
-  end
-end
-
-class Square
-  INITIAL_MARKER = " "
-
-  attr_accessor :marker
-
-  def initialize(marker=INITIAL_MARKER)
-    @marker = marker
-  end
-
-  def to_s
-    @marker
-  end
-
-  def marked?
-    marker != INITIAL_MARKER
-  end
-
-  def unmarked?
-    marker == INITIAL_MARKER
-  end
-end
-
-class Player
-  attr_reader :marker
-
-  def initialize(marker)
-    @marker = marker
-  end
-end
+require_relative 'board'
+require_relative 'player'
 
 class TTTGame
   HUMAN_MARKER = 'X'
   COMPUTER_MARKER = 'O'
   FIRST_TO_MOVE = HUMAN_MARKER
 
-  attr_reader :board, :human, :computer
+  attr_reader :board, :human, :computer, :score
 
   def initialize
     @board = Board.new
     @human = Player.new(HUMAN_MARKER)
     @computer = Player.new(COMPUTER_MARKER)
     @current_marker = FIRST_TO_MOVE
+    @score = {@human => 0, @computer => 0}
   end
 
   def reset
@@ -197,6 +108,14 @@ class TTTGame
     answer == 'y'
   end
 
+  def increment_score(score, player)
+    score[player] += 1
+  end
+
+  def display_score(score)
+    puts "Your score: #{score[@human]}, Computer score: #{score[@computer]}"
+  end
+
   def play
     clear
     display_welcome_message
@@ -211,6 +130,8 @@ class TTTGame
       end
 
       display_result
+      increment_score(score)
+      display_score(score)
       break unless play_again?
       reset
     end
