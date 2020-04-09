3dc4b2af0802b7a4f11b2570cf306fe14f05af63
Cameron Nigro <cameron.nigro@gmail.com>
2020-04-08 21:27:44 -0400

    really sad circular queue implementation

--- object_oriented_programming/medium_1/students.rb
+++ object_oriented_programming/medium_1/students.rb
@@ -28,3 +28,10 @@ class Undergraduate < Student
     super
   end
 end
+
+# Further Exploration
+# There is one other "form" of the keyword super. We can call it as super().
+# This calls the superclass method of the same name as the calling method,
+# but here no arguments are passed to the superclass method at all.
+
+# Can you think of a way to use super() in another Student related class?
